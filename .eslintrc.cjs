bbbf4350d636eb69c5ab0aa0d0ede341fce36810
Alex Khvostov <alexandr.khvostov@sysgears.com>
2025-03-31 13:22:33 +0300

    chore: update ESLint configuration to include .eslintrc.cjs in ignore patterns

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,9 @@
 // This configuration only applies to the package manager root.
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  ignorePatterns: ["apps/**", "packages/**"],
-  extends: [require.resolve("@repo/eslint-config/library.cjs")],
-  parser: "@typescript-eslint/parser",
+  ignorePatterns: ['apps/**', 'packages/**', '.eslintrc.cjs'],
+  extends: [require.resolve('@repo/eslint-config/library.cjs')],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     project: true,
   },
